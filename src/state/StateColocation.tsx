a297a8234880d96fef55e954a0bb0a6cf3a63f70
andre.faria <andre.faria@ceiia.com>
2022-12-07 12:31:22 +0000

    Revert "a"
    This reverts commit b6071be722a152e219f7df7d3ec6649e34bbb008.

--- src/state/StateColocation.tsx
+++ src/state/StateColocation.tsx
@@ -5,8 +5,8 @@ import { useState } from "react"
 /**
  * Neste caso apenas o componente Name utiliza o parametro name, 
  * Assim em vez de ser gerido pelo componente Pai é gerido pelo proprio componente
- * cada vez q o nome altera, nao renderiza o componente todo
- * State Colocation 
+ * 
+ * State Colocation
  */
 
 function Name() {
