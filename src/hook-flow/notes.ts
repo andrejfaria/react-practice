395eca3a3aa68cce91064763f340d07a815f29ba
andre.faria <andre.faria@ceiia.com>
2022-12-07 09:56:52 +0000

    useEffect hookFlow

---
+++ src/hook-flow/notes.ts
@@ -0,0 +1,20 @@
+/*
+  #############  React Hook   ###################
+  1. Lazy initializers on mount, first time page loads
+  2.Render
+  3. React updates DOM
+  4. Cleanup LayoutEffects
+ 5.Run LayoutEffects
+  6.Browse paints screen
+  7.CleanEffects
+  8. Run Effects
+
+
+
+  ARRAY DE DEPENDENCIAS NO USE EFFECT
+  sem [] => depende de tudo
+  com um [] vazio => não depende de ninguém 
+  com [] preenchido => [depende de alguem]
+
+  
+*/
